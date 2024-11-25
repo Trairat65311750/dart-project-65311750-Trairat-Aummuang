e2e115db1a09507533e06dbdd6bda941b73441b0
Trairat65311750 <trairata65@nu.ac.th>
2024-11-25 15:52:26 +0700

    question2-1.dart

---
+++ question2-1.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main() {
+  print('enter number: ');
+  int number = int.parse(stdin.readLineSync()!);
+
+  if (number % 2 == 0) {
+    print("$number is even");
+  } else {
+    print("$number is odd");
+  }
+}
---
+++ question2-2.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+void main() {
+  print('Please enter a character:');
+  String character = stdin.readLineSync()!;
+
+  String vowels = 'aeiouAEIOU';
+
+  if (character.length == 1) {
+    if (vowels.contains(character)) {
+      print('$character is a vowel.');
+    } else if (RegExp(r'[a-zA-Z]').hasMatch(character)) {
+      print('$character is a consonant.');
+    } else {
+      print('Please enter a valid alphabetic character.');
+    }
+  } else {
+    print('Please enter only one character.');
+  }
+}
